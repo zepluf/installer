72ca8c89240101fab32697854858abeb515564ee
Raine Nguyen <vunguyen@rubikin.com>
2012-12-30 14:13:09 +0700

    init

---
+++ Plugin/Installer.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by RubikIntegration Team.
+ * Date: 12/30/12
+ * Time: 2:02 PM
+ * Question? Come to our website at http://rubikintegration.com
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zepluf\Installer\Plugin;
+
+use Composer\Package\PackageInterface;
+use Composer\Installer\LibraryInstaller;
+
+class Installer extends LibraryInstaller {
+    /**
+     * {@inheritDoc}
+     */
+    public function getInstallPath(PackageInterface $package) {
+        $prefix = substr ($package->getPrettyName (), 0, 17);
+        if ('zepluf/plugin-' !== $prefix) {
+            throw new \InvalidArgumentException(
+                'Unable to install plugin, ZePLUF plugin package names  should always begin with "zepluf/plugin-"'
+            );
+        }
+
+        return 'app/plugins/'.substr($package->getPrettyName(), 17);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function supports($packageType) {
+        return 'zepluf-plugin' === $packageType;
+    }
+}
