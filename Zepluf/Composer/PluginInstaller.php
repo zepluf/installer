19d180930b4a8aabf11627552b153ad6a786dca4
Raine Nguyen <vunguyen@rubikin.com>
2012-12-30 17:46:30 +0700

    minor structure change

--- Zepluf/Composer/PluginInstaller.php
+++ Zepluf/Composer/PluginInstaller.php
@@ -8,7 +8,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Zepluf\Installer;
+namespace Zepluf\Composer;
 
 use Composer\Package\PackageInterface;
 use Composer\Installer\LibraryInstaller;
